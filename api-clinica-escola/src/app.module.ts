906a6d159d4772b7a194a6c30dbcf98e4a8eb537
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2026-06-28 13:42:16 -0300

    Criação dos arquivos para os atendimentos + DTO

--- api-clinica-escola/src/app.module.ts
+++ api-clinica-escola/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AtendimentosModule } from './atendimentos/atendimentos.module';
 
 @Module({
-  imports: [],
+  imports: [AtendimentosModule],
   controllers: [AppController],
   providers: [AppService],
 })
