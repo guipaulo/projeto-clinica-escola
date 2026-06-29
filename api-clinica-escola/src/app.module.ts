8d44ee92254285ad6d40063f14bbc15f3ac99ca8
zehelll <aviz.h@escolar.ifrn.edu.br>
2026-06-28 23:12:45 -0300

    correçao do app.module para incluir os modulos de serviços e horarios

--- api-clinica-escola/src/app.module.ts
+++ api-clinica-escola/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServicosModule } from './servicos/servicos.module';
+import { HorariosModule } from './horarios/horarios.module';
 
 @Module({
   imports: [ServicosModule, HorariosModule],
