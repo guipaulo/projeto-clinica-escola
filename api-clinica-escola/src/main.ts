32155afe9d2b98a9b2b0afb346cf41d79f9f280d
nicoletsi <nicole.c@escolar.ifrn.edu.br>
2026-07-16 02:27:17 -0300

    adição do import swagger e instalação das dependências passport-local @nestjs/swagger e --save-dev @types/passport-local

--- api-clinica-escola/src/main.ts
+++ api-clinica-escola/src/main.ts
@@ -1,3 +1,4 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
